f15ccee63b07c7980b7d4e163aa5b923365efd2d
Hank McCord <hymccord@gmail.com>
2022-11-07 09:48:05 -0500

    wip

---
+++ src/scripts/environment.service.ts
@@ -0,0 +1,14 @@
+export class EnvironmentService {
+    protected baseUrl: string;
+    protected dbUrl: string;
+    protected mapIntakeUrl: string;
+    protected convertibleIntakeUrl: string;
+    protected mapHelperUrl: string;
+    protected rhIntakeUrl: string;
+    // const base_domain_url = process.env.BACKEND_URL;
+    // const db_url = base_domain_url + "/intake.php";
+    // const map_intake_url = base_domain_url + "/map_intake.php";
+    // const convertible_intake_url = base_domain_url + "/convertible_intake.php";
+    // const map_helper_url = base_domain_url + "/maphelper.php";
+    // const rh_intake_url = base_domain_url + "/rh_intake.php";
+}
